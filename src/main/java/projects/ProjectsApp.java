86426294c60064df5fbf3c7eaa2eaa47af61c5f7
Brandee McKinney <brandeelmckinney@gmail.com>
2022-11-05 09:50:32 -0500

    Week 9 push 2

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -1,13 +1,17 @@
 package projects;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
+import projects.entity.Project;
 import projects.exception.DbException;
+import projects.service.ProjectService;
 
 public class ProjectsApp {
 	private Scanner scanner = new Scanner(System.in);
+	private ProjectService projectService = new ProjectService();
 	
 		// @formatter:off 
 		private List<String> operations = List.of(
@@ -32,7 +36,7 @@ public class ProjectsApp {
 						createProject();
 						break;
 					default:
-						System.out.println("\n" + selection + "is not a valid selection. Try again.");
+						System.out.println("\n" + selection + " is not a valid selection. Try again.");
 						break;
 					}
 				}
@@ -48,7 +52,23 @@ public class ProjectsApp {
 			return true;
 		}
 		
-		private void createProject() {			
+		private void createProject() {
+			String projectName = getStringInput("Enter the project name");
+			BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours");
+			BigDecimal actualHours = getDecimalInput("Enter the actual hours");
+			Integer difficulty = getIntInput("Enter the project difficulty (1-5)");
+			String notes = getStringInput("Enter the project notes");
+			
+			Project project = new Project();
+			
+			project.setProjectName(projectName);
+			project.setEstimatedHours(estimatedHours);
+			project.setActualHours(actualHours);
+			project.setDifficulty(difficulty);
+			project.setNotes(notes);
+			
+			Project dbProject = projectService.addProject(project);
+			System.out.println("You have successfully created project: " + dbProject);
 		}
 		
 		private int getUserSelection() {
@@ -86,7 +106,21 @@ public class ProjectsApp {
 			
 			return input.isBlank() ? null : input.trim();
 		}
-
+		
+		private BigDecimal getDecimalInput (String prompt) {
+			String input = getStringInput(prompt);
+			
+			if(Objects.isNull(input)) {
+				return null;
+			}
+			
+			try {
+				return new BigDecimal(input).setScale(2);
+			}
+			catch(NumberFormatException e) {
+				throw new DbException(input + " is not a valid decimal number.");
+			}
+		}
 }	
 
 
